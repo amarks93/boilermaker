37be8cf108291d8aa4e88039ac6388437347adc7
Alexandra Marks <amarks93@gmail.com>
2021-09-13 06:07:47 -0400

    fixing db for heroku

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,24 @@
 const Sequelize = require('sequelize');
 
+const config = {
+  logging: false,
+};
+
+if (process.env.LOGGING === 'true') {
+  delete config.logging;
+}
+
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
+
 const db = new Sequelize(
   process.env.DATABASE_URL || 'postgres://localhost:5432/yourdbname',
-  {
-    logging: false,
-  }
+  config
 );
 
 module.exports = db;
